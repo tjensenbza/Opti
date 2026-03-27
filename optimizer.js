b6de96bedd3765d2c58f42e28a7ca09701501c3f
tjensenbza <t.jensen@semcoglas.de>
2026-03-27 20:21:01 +0100

    Refactor optimizeWithRemnants function
    Refactor optimizeWithRemnants function for readability.

--- optimizer.js
+++ optimizer.js
@@ -1,17 +1,17 @@
-function optimizeWithRemnants(sheet, cuts) {
+function optimizeWithRemnants(sheet,cuts){
 
-  cuts.sort((a, b) => (b.w * b.h) - (a.w * a.h));
+cuts.sort((a,b)=>(b.w*b.h)-(a.w*a.h));
 
-  let sheets = [];
+let sheets=[];
 
-  while (cuts.length > 0) {
+while(cuts.length>0){
 
-    let placed = guillotinePack(sheet, cuts);
+let placed=guillotinePack(sheet,cuts);
 
-    sheets.push(placed);
+sheets.push(placed);
 
-    cuts = cuts.filter(c => !placed.includes(c));
-  }
+cuts=cuts.filter(c=>!placed.includes(c));
+}
 
-  return sheets;
+return sheets;
 }
